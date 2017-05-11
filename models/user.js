bd24ec6523e451b7476d2f769398a8858d2a835a
etracha <etracha@hotmail.com>
2017-05-11 15:32:35 +0700

    Author Added

--- models/user.js
+++ models/user.js
@@ -19,6 +19,18 @@ const UserSchema = mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    readLevel: {
+        type: Number,
+        default: 0
+    },
+    writeLevel: {
+        type: Number,
+        default: 0
+    },
+    adminLevel: {
+        type: Number,
+        default: 0
     }
     
 });
@@ -49,4 +61,20 @@ module.exports.comparePassword = function(candidatePassword, hash, callback){
         if(err) throw err;
         callback(null, isMatch);
     });
+}
+
+module.exports.getReadAccessible = function(accUser, page_id, callback){
+    callback = !!(accUser.readLevel & 1 << page_id);
+}
+
+module.exports.setReadAccessible = function(accUser, page_id, accessible, callback){
+    accUser.readLevel = accUser.readLevel.toString(2);  // base 2 number
+    accUser.readLevel = accUser.readLevel.split('').reverse().join(''); // string reverse
+
+    accUser.readLevel[page_id] = accessible;
+
+    accUser.readLevel = accUser.readLevel.split('').reverse().join(''); // string reverse
+    accUser.readLevel = accUser.readLevel.toString(10);  // base 10 number
+
+    accUser.save(callback);
 }
--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.post('/register', function(req, res, next){
        name: req.body.name,
        email: req.body.email,
        username: req.body.username,
-       password: req.body.password
+       password: req.body.password,
    });
 
    User.addUser(newUser, function(err, user){
@@ -49,7 +49,7 @@ router.post('/authenticate', function(req, res, next){
                        id: user._id,
                        name: user.name,
                        username: user.username,
-                       email: user.email
+                       email: user.email,
                    }
                });
 
